49d737eae39b7f7de0cb418ec7be555c29bc3c4d
Andrew Rusli <andreworusli@gmail.com>
2021-04-30 21:33:02 -0400

    refactored and simplified state of ListItem into two css classes

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,37 +4,32 @@ import {MdRemoveCircle} from 'react-icons/md';
 
 const ListItem = ({item, list, setList, isLoggedIn, setIsLoggedIn, userId, setUserId, makeDeleteRequest}) => {
     console.log('ListItem Component Renders');
-    const [checked, setChecked] = useState(false);
+    // const [checked, setChecked] = useState(false);
     //styling states
-    const [maxHeight, setMaxHeight] = useState('150px');
-    const [padding, setPadding] = useState('10px');
+    const [classOpen, setClassOpen] = useState('open')
+    const [classChecked, setClassChecked] = useState('unchecked');
     const [display, setDisplay] = useState('grid');
-    const [classVar, setClassVar] = useState('unchecked');
-    //useref
-    const checkState = useRef(checked);
+ 
+    // const [maxHeight, setMaxHeight] = useState('150px');
+    // const [padding, setPadding] = useState('10px');
     
  
-    const checkOff = async (e) => {
+    const checkOff = (e) => {
         e.preventDefault();
-        if (!checked) {
-            setClassVar('checked')
-            await setChecked(true);
-            console.log(checkState.current); //why false in react dev tools/console?
+        if (classChecked === 'unchecked') {
+            setClassChecked('checked')
         }
-        if (checked) {
-            setClassVar('unchecked')
-            setChecked(false);
-            console.log(checked);
-            
+
+        if (classChecked === 'checked') {
+            setClassChecked('unchecked')
         }
     };
 
     //UPDATE LIST TEST
      const updateList = () => {
-        setMaxHeight('0px'); 
-        setPadding('0px')
+        setClassOpen('closed');
         setTimeout(()=>setDisplay('none'), 190); //prevents weird css spacingbackup when deleting multiple items.
-        //update list state minus the term, no get request. - katie
+        //update list state minus the term, no get request. - katie.
         console.log('item:',item);
         makeDeleteRequest(item);
     };
@@ -42,14 +37,12 @@ const ListItem = ({item, list, setList, isLoggedIn, setIsLoggedIn, userId, setUs
     //Remember: one list item per component, each with their own personal states.
     //Remember: only the targeted list item will re-render on listitem state change.
    
-    // just use regular variable to change classname. - katie. (this didn't work for me.)
+    // just use regular variable to change classname. - katie. (this didn't work for me?)
     return (
         <li 
-        className={classVar}
+        className={`${classChecked} ${classOpen}`}
         style={{
             display: display,
-            maxHeight: maxHeight,
-            padding: padding
             }}>
             <div style={{overflowWrap: 'break-word', margin: '0 10px'}}>{item.content}</div>
             <button style={{color: '#ffffcc', backgroundColor: '#0080ff', fontWeight: 'bold', height: '2.5rem', cursor: 'pointer', borderRadius: '5px', borderStyle: 'none'}} onClick={checkOff} ><FaCheckCircle size="1.4rem" /></button>
